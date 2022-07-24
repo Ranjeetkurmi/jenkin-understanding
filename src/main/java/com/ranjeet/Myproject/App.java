2ad26b6cf141728fba8090f470ee41c3a3c9e2d3
ranjkurm <ranjkurm@DVCINH6086658.corp.amdocs.com>
2022-07-24 16:52:18 +0530

    intial commit

---
+++ src/main/java/com/ranjeet/Myproject/App.java
@@ -0,0 +1,13 @@
+package com.ranjeet.Myproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+       System.out.println("this is my second jenkin project");
+    }
+}
